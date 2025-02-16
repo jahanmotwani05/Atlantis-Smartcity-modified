21b3f5672257163ebf375e5eec4e6a589df01e6d
Abhinav Pillai <abhinavpillai92@gmail.com>
2025-02-16 15:01:51 +0530

    app.tsx fix landing page showing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Landing from './components/Landing';
 import Login from './components/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<div>Dashboard</div>} />
       </Routes>
     </Router>
